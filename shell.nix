7e668b3c9e70b7594d5a165334aad505aa1ae8f9
chyna-gvng <angoyewally@outlook.com>
2026-03-08 21:58:39 +0300

    mod: shell.nix

--- shell.nix
+++ shell.nix
@@ -4,9 +4,21 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     python312
     uv
+    pango
+    glib
+    cairo
+    fontconfig
+    harfbuzz
   ];
 
   shellHook = ''
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath (with pkgs; [
+      pango
+      glib
+      cairo
+      fontconfig
+      harfbuzz
+    ])}
     export OPENROUTER_API_KEY="{YOUR_KEY}"
   '';
 }
